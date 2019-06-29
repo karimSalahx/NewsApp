baf6fb70a13e71c06a995715e90adc41a6c98d56
bestever255 <[karemmohamed2002@gmail.com]>
2019-06-29 18:41:35 -0400

    Initial commit

---
+++ app/src/main/java/com/best/karem/newsapp/MainActivity.java
@@ -0,0 +1,81 @@
+package com.best.karem.newsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.best.karem.newsapp.Model.Article;
+import com.best.karem.newsapp.Model.News;
+import com.best.karem.newsapp.api.ApiClient;
+import com.best.karem.newsapp.api.ApiInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String API_KEY = "bfe51c17ab2d4dbcbd430be5eef6c29a";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private Adapter adapter;
+    private String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+
+        loadJSON();
+
+    }
+
+
+    public void loadJSON() {
+
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        String country = Utils.getCountry();
+
+        Call<News> call;
+        call = apiInterface.getNews(country, API_KEY);
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (response.isSuccessful() && response.body().getArticle() != null) {
+
+                    if (!articles.isEmpty()) {
+                        articles.clear();
+                    }
+                    articles = response.body().getArticle();
+                    adapter = new Adapter(articles, MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+                } else {
+                    Toast.makeText(MainActivity.this, "No Result", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/best/karem/newsapp/api/ApiInterface.java
@@ -0,0 +1,17 @@
+package com.best.karem.newsapp.api;
+
+import com.best.karem.newsapp.Model.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("top-headlines")
+    Call<News> getNews(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+
+}
